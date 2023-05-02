a959ec60f47057d5fa1c9cefc862c816f683f530
William Gan <hswg94@gmail.com>
2023-05-02 19:55:41 +0800

    temporarily removed secure flag

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ const sessionConfig = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Set secure flag if in production
+    //secure: process.env.NODE_ENV === "production", // Set secure flag if in production
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7, // Set cookie expiration time to 1 week from now
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
